1f6f2288663642ef7a1e3a8fb0d5d5fb561643bf
Mike Jumper <mjumper@apache.org>
2022-10-17 15:48:40 -0700

    Replace Jekyll site with a bunch of swimming fish.

---
+++ fish.js
@@ -0,0 +1,32 @@
+var addFish = function addFish() {
+
+    var fish = document.createElement('div');
+    fish.className = 'fish';
+    fish.textContent = '\u{1F41F}';
+    fish.style.setProperty('--layer', Math.floor(Math.random() * 20) + 1);
+    fish.style.setProperty('--color', Math.random());
+    fish.style.top = Math.random() * 100 + '%';
+
+    document.body.appendChild(fish);
+    fish.addEventListener('animationend', function removeFish() {
+        fish.parentNode.removeChild(fish);
+    });
+
+};
+
+var scheduleFish = function scheduleFish() {
+    window.setTimeout(function populateFish() {
+        window.requestAnimationFrame(addFish);
+        scheduleFish();
+    }, Math.random() * 5000);
+};
+
+for (var i = 0; i < 20; i++) {
+    var water = document.createElement('div');
+    water.className = 'water';
+    water.style.setProperty('--layer', i);
+    document.body.appendChild(water);
+}
+
+scheduleFish();
+
--- index.markdown
+++
@@ -1,9 +0,0 @@
----
-# Feel free to add content and custom Front Matter to this file.
-# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-
-layout: home
----
-
-<a href="https://www.buymeacoffee.com/mjumper" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;"></a>
-
