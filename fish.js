c579e373bc8aec979d8d52bc35716aa3f3972e0f
Michael Jumper <mjumper@apache.org>
2024-02-22 13:31:18 -0800

    Enforce a minimum wait time on the rate fish are added.

--- fish.js
+++ fish.js
@@ -32,12 +32,32 @@ const MAX_FISH_DEPTH = 20;
 const MAX_FISH_INTERVAL = 5000;
 
 /**
+ * The minimum number of milliseconds to wait before adding a new fish.
+ */
+const MIN_FISH_INTERVAL = 500;
+
+/**
+ * The timestamp that the last fish was added, in milliseconds since epoch
+ * (midnight at the beginning of 1970-01-01, UTC).
+ */
+var lastFishAdded = 0;
+
+/**
  * Adds a new fish to the set of fish currently displayed. The new fish will be
  * automatically assigned a random location, color, and depth. Once the fish
  * has swum all the way across the screen, it will be automatically removed
- * from the DOM.
+ * from the DOM. If insufficient time has elapsed since the last time this
+ * function was called (based on MIN_FISH_INTERVAL), this function has no
+ * effect.
  */
 const addFish = function addFish() {
+
+    // Do not allow fish to be added faster than MIN_FISH_INTERVAL
+    let timestamp = Date.now();
+    if (timestamp - lastFishAdded < MIN_FISH_INTERVAL)
+        return;
+
+    lastFishAdded = timestamp;
 
     let fish = document.createElement('div');
     fish.className = 'fish';
@@ -58,18 +78,16 @@ const addFish = function addFish() {
  *
  * NOTE: There is no explicit upper bound on the number of fish, however there
  * is an implicit upper bound given that there is an upper bound for how long
- * each fish will remain (they will eventually reach the other side and be
- * removed, and there is a maximum length of time that this can take) as well
- * as a statistical upper bound on the number of times addFish() will be
- * invoked over the course of that time (the function is called at random
- * intervals). If the JavaScript engine decides to give us poorly-distributed
- * fish intervals, then that upper bound vanishes.
+ * each fish will remain in the DOM (they will eventually reach the other side
+ * and be removed, and there is a maximum length of time that this can take) as
+ * well as an upper bound on the number of times addFish() will have an effect
+ * over the course of that time (MIN_FISH_INTERVAL).
  */
 const scheduleFish = function scheduleFish() {
     window.setTimeout(function populateFish() {
         window.requestAnimationFrame(addFish);
         scheduleFish();
-    }, Math.random() * MAX_FISH_INTERVAL);
+    }, Math.random() * (MAX_FISH_INTERVAL - MIN_FISH_INTERVAL) + MIN_FISH_INTERVAL);
 };
 
 // Add layers of water corresponding to the maximum fish depth
